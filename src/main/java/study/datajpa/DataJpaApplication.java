3382140fb51e50eefd84a61021c8942499ce9410
dbslzld15 <dbslzld15@naver.com>
2022-03-13 01:57:21 +0900

    BaseEntity 사용, Auditing 관련 예제 추가

--- src/main/java/study/datajpa/DataJpaApplication.java
+++ src/main/java/study/datajpa/DataJpaApplication.java
@@ -2,7 +2,14 @@ package study.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class DataJpaApplication {
 
@@ -10,4 +17,8 @@ public class DataJpaApplication {
 		SpringApplication.run(DataJpaApplication.class, args);
 	}
 
+	@Bean
+	public AuditorAware<String> auditorProvider() { //@CreatedBy, @LastModifiedBy에 값을 넣어주기 위함
+		return () -> Optional.of(UUID.randomUUID().toString());
+	}
 }
