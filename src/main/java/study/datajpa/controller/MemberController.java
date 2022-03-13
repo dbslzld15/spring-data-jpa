be7600ed1235f627a4e24af59a869a5ff999523b
dbslzld15 <dbslzld15@naver.com>
2022-03-13 18:11:39 +0900

    Pageable을 이용해 페이징 api 만드는법 추가

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -1,9 +1,13 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -24,6 +28,13 @@ public class MemberController {
     @GetMapping("/members2/{id}")
     public String findMember2(@PathVariable("id") Member member) { //도메인 클래스 컨버터 작동
         return member.getUsername();
+    }
+
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5, sort = "username") Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable);//Pageable이 들어가면 요청파라미터에 의해 페이지 사이즈, 정렬 등을 사용 할 수 있음
+        Page<MemberDto> map = page.map(member -> new MemberDto(member.getId(), member.getUsername(), null));
+        return map;
     }
 
     @PostConstruct
